99ea1ff7fa46762c6b4a164358c936ceb27e6dd6
Thumrith R <thumrithr@gmail.com>
2021-06-23 01:12:51 +1000

    Defining long values
    Defining long values and correcting incorrect comment values

--- user_upload.php
+++ user_upload.php
@@ -10,13 +10,21 @@
 // define and gather the command line options using getopt -- START
 
 // defining short values
-$shortoptions  = "u:"; // -u to gather the database username
-$shortoptions  .= "p:"; // -u to gather the database password
-$shortoptions  .= "h:"; // -u to gather the database host
-$shortoptions  .= "n:"; // -u to gather the database name, this is an option value as the database name will be set to userdetails
+$shortoptions  = "u:"; 	// -u to gather the database username
+$shortoptions  .= "p:"; // -p to gather the database password
+$shortoptions  .= "h:"; // -h to gather the database host
+$shortoptions  .= "n:"; // -n to gather the database name, this is an optional value as the database name will be set to userdetails
+
+// defining long values
+$longoptions  = array(
+    "file:",     		// Gets the passed csv file that needs to be parsed
+    "help",        		// executes the help section and displays to the user
+    "create_table",  	// executes table creation process
+    "dry_run", 			// executes dry run process but is dependent on the file input
+);
 
 // gathering options
-$options = getopt($shortoptions);
+$options = getopt($shortoptions, $longoptions);
 
 var_dump($options);
 
